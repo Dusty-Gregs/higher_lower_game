300816e589cd735d5ee86793ae76a7c1ef0be06f
Dusty-Gregs <musdisney@gmail.com>
2023-09-11 20:20:12 -0500

    Reformatted python

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from game_data import data
 import random
 import os
 
+
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
+
 
 def format_data(account):
     #Format the account data into a printable format
@@ -12,6 +14,7 @@ def format_data(account):
     account_descr = account["description"]
     account_country = account["country"]
     return f"{account_name}, a {account_descr}, from {account_country}"
+
 
 def check_answer(guess, a_followers, b_followers):
     #Take the user guess and follower counts and returns if they got it right.
@@ -40,17 +43,15 @@ while game_should_continue:
     print(vs)
     print(f"Compare B: {format_data(account_b)}")
 
-
-
     # Ask user for a guess.
     guess = input("Who has more followers? Type 'A' or 'B':").lower()
 
     # Check if user is correct.
-    ## Get follower count for each account.
+    # Get follower count for each account.
     a_follower_count = account_a["follower_count"]
     b_follower_count = account_b["follower_count"]
 
-    ## Use if statement to check if user is correct.
+    # Use if statement to check if user is correct.
     is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
     # Clear the screen between rounds.
